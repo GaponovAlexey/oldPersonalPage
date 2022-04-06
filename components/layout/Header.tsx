0cf507ce0f293a12464f994c93cba2b58f8b79c1
Kekelberi <alexei.gaponov@gmail.com>
2022-04-06 21:00:50 +0300

    save

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,6 +1,14 @@
 import s from '../../styles/Home.module.scss'
 const Header = () => {
-  return <div className={s.componentheader}>Personal</div>
+  return (
+    <div className={s.componentHeader}>
+      <h1>Personal</h1>
+      <ul>
+        <li>home</li>
+        <li>workshop</li>
+      </ul>
+    </div>
+  )
 }
 
 export default Header
