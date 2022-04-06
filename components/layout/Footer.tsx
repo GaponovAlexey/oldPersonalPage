6503d5993f1d4f1c44be6b4582e0ce237dd65ac5
Kekelberi <alexei.gaponov@gmail.com>
2022-04-06 22:24:40 +0300

    save

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,6 +1,12 @@
 import s from '../../styles/Home.module.scss'
 const Footer = () => {
-  return <div className={s.footer}>footer</div>
+  return (
+    <div className={s.footer}>
+      <h4>created by Alexey Gaponov</h4>
+      <p>alexei.gaponov@gmail.com ©2022 portfolio website</p>
+      <a href='https://t.me/g_alexey_ur'>t.me</a>
+    </div>
+  )
 }
 
 export default Footer
