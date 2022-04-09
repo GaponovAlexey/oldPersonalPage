9e49f483b49ae238f2fc6da958087e6f00b22264
Kekelberi <alexei.gaponov@gmail.com>
2022-04-09 19:45:21 +0300

    save

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,10 +1,19 @@
 import Image from 'next/image'
-
+import { motion, useMotionValue, useTransform } from 'framer-motion'
 const Main = () => {
-
+  const x = useMotionValue(0)
+  const scaleZ = useTransform(x, (v) => v / 100)
   return (
-    <div  >
+    <div className='flex'>
       <Image src='/jpg/1.png' width='540%' height='720%' />
+      <div className='flex-wrap pl-40 ml-20 '>
+        test
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        />
+      </div>
     </div>
   )
 }
