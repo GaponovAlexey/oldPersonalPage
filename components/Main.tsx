b0e6e97318f69cee602b24508dad9cf7bb37774d
Kekelberi <alexei.gaponov@gmail.com>
2022-04-09 19:02:37 +0300

    save

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,18 +1,12 @@
 import Image from 'next/image'
-import { useEffect } from 'react'
 
 const Main = () => {
-  const Images = (
-    <Image className='h-50' src='/jpg/1.png' width='540%' height='720%' />
-  )
-  let newImage = null
-  useEffect(() => {
-    setInterval(() => {
-      const newImage = Images
-    }, 1000)
-  }, [])
 
-  return <div>{newImage}</div>
+  return (
+    <div  >
+      <Image src='/jpg/1.png' width='540%' height='720%' />
+    </div>
+  )
 }
 
 export default Main
