77f14e25116b47d44804a421a06dac57116ff9ce
Kekelberi <alexei.gaponov@gmail.com>
2022-04-14 01:36:30 +0300

    save

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 import s from '../styles/Home.module.scss'
+import Testing from './testing.tsx'
 const base = [
   { title: "Hi there 👋, I'm Alexey'" },
   { title: 'I am a Web Developer' },
@@ -22,62 +23,67 @@ const about = [
 ]
 const Home = () => {
   return (
-    <div className={s.main}>
-      <strong>
-        <motion.div
-          initial={{ x: -1000 }}
-          animate={{ x: 0 }}
-          transition={{
-            type: 'tween',
-            duration: 0.3,
-          }}
-        >
-          About me
-        </motion.div>
-      </strong>
+    <>
+      <div className='fixed z-[1] bottom-0 right-0'>
+        <Testing />
+      </div>
+      <div className={s.main}>
+        <strong>
+          <motion.div
+            initial={{ x: -1000 }}
+            animate={{ x: 0 }}
+            transition={{
+              type: 'tween',
+              duration: 0.3,
+            }}
+          >
+            About me
+          </motion.div>
+        </strong>
 
-      <div className={s.about}>
-        <motion.div
-          transition={{ type: 'tween' }}
-          initial={{ opacity: 0, x: -300 }}
-          animate={{ opacity: 1, x: 0 }}
-        >
-          <Image src='/jpg/1.png' width='455%' height='620%' />
-        </motion.div>
-        <div>
-          <strong>
-            <ul>
-              {base.map((el, i) => (
-                <motion.li
-                  key={i}
-                  initial={{ opacity: 0, x: +300 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{ type: 'tween', delay: i * 0.04 }}
-                >
-                  {el.title}
-                </motion.li>
-              ))}
-            </ul>
-          </strong>
-          <br />
-          <br />
-          <strong>
-            <ul>
-              {about.map((el, i) => (
-                <motion.li
-                  key={i}
-                  initial={{ opacity: 0, y: +300 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ type: 'tween', delay: i * 0.04 }}
-                >
-                  <text>{el.title}</text>
-                </motion.li>
-              ))}
-            </ul>
-          </strong>
+        <div className={s.about}>
+          <motion.div
+            transition={{ type: 'tween' }}
+            initial={{ opacity: 0, x: -300 }}
+            animate={{ opacity: 1, x: 0 }}
+          >
+            <Image src='/jpg/1.png' width='455%' height='620%' />
+          </motion.div>
+          <div>
+            <strong>
+              <ul>
+                {base.map((el, i) => (
+                  <motion.li
+                    key={i}
+                    initial={{ opacity: 0, x: +300 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ type: 'tween', delay: i * 0.04 }}
+                  >
+                    {el.title}
+                  </motion.li>
+                ))}
+              </ul>
+            </strong>
+            <br />
+            <br />
+            <strong>
+              <ul>
+                {about.map((el, i) => (
+                  <motion.li
+                    key={i}
+                    initial={{ opacity: 0, y: +300 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ type: 'tween', delay: i * 0.04 }}
+                  >
+                    <text>{el.title}</text>
+                  </motion.li>
+                ))}
+              </ul>
+            </strong>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- pages/testing.tsx
+++ pages/testing.tsx
@@ -1,5 +1,43 @@
-const Testing = () => {
-  return <div>test1</div>
+import { Physics, usePlane } from '@react-three/cannon'
+import { OrbitControls } from '@react-three/drei'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import type { NextPage } from 'next'
+import { Suspense, useRef, useState } from 'react'
+
+function Box(props) {
+  const ref = useRef()
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+const Testing: NextPage = () => {
+  return (
+    <div>
+      <Suspense fallback={null}>
+        <Canvas>
+          <Physics>
+            <ambientLight intensity={0.1} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[+1.2, 0, 0]} />
+          </Physics>
+        </Canvas>
+      </Suspense>
+    </div>
+  )
 }
 
 export default Testing
