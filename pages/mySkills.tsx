043cc94f58d74bbd1580d6a125441e81eacb1c3b
Kekelberi <alexei.gaponov@gmail.com>
2022-04-13 23:49:31 +0300

    save

--- pages/mySkills.tsx
+++ pages/mySkills.tsx
@@ -1,113 +1,120 @@
 import { motion } from 'framer-motion'
 import s from '../styles/Home.module.scss'
+// export const getStaticProps = async () => {
+//   const response = await fetch()
+//   const data = await response.json()
+//   if(!data) {return {notFound:true}
+//   return {props: { contacts: data },}
+// }
+
 const Skills = () => {
-  const frontend = [
-    { title: 'JS | TS' },
-    { title: 'HTML | CSS | Sass' },
-    { title: 'React | NextJs' },
-    { title: 'React Hooks' },
-    { title: 'React Context API' },
-    { title: 'Redux Toolkit' },
-    { title: 'Redux Saga' },
-    { title: 'Redux Persist' },
-    { title: 'RTK Query' },
-    { title: 'React Router6' },
-    { title: 'React Transition Group' },
-    { title: 'React Hook Form' },
-    { title: 'Slick' },
-    { title: 'PassportJs' },
-    { title: 'TailwindCSS' },
-    { title: 'Styled-Components' },
-    { title: 'Emotion' },
-  ]
+  const skills = {
+    frontend: [
+      { title: 'JS | TS' },
+      { title: 'HTML | CSS | Sass' },
+      { title: 'React | NextJs' },
+      { title: 'React Hooks' },
+      { title: 'React Context API' },
+      { title: 'Redux Toolkit' },
+      { title: 'Redux Saga' },
+      { title: 'Redux Persist' },
+      { title: 'RTK Query' },
+      { title: 'React Router6' },
+      { title: 'React Transition Group' },
+      { title: 'React Hook Form' },
+      { title: 'Slick' },
+      { title: 'PassportJs' },
+      { title: 'TailwindCSS' },
+      { title: 'Styled-Components' },
+      { title: 'Emotion' },
+    ],
+    backend: [
+      { title: 'NodeJS' },
+      { title: 'Express' },
+      { title: 'RESTful API' },
+      { title: 'SQL' },
+      { title: 'PostgreSQL' },
+      { title: 'Firebase' },
+      { title: 'Supabase' },
+      { title: 'MongoDB' },
+      { title: 'Mongoose' },
+      { title: 'GraphQL' },
+      { title: 'Apollo Client' },
+      { title: 'Apollo Server' },
+    ],
+    uiux: [
+      { title: 'Figma' },
+      { title: 'Framer motion' },
+      { title: 'PixiJS' },
+      { title: 'Materialize' },
+      { title: 'Ant Design of React' },
+      { title: 'Material-UI' },
+      { title: 'Material Design 3' },
+      { title: 'React-Vis' },
+      { title: 'UIkit' },
+      { title: 'Chart.js' },
+      { title: 'React-Motion' },
+      { title: 'React Materialize' },
+      { title: 'React Bootstrap' },
+      { title: 'React-Spring' },
+      { title: 'Tailblocks' },
+    ],
+    analytics: [
+      { title: 'Python' },
+      { title: 'Amplitude' },
+      { title: 'Tableau' },
+      { title: 'Power BL' },
+      { title: 'Highcharts' },
+      { title: 'GoJs' },
+      { title: 'Google Excel' },
+      { title: 'Pandas' },
+      { title: 'Numpy' },
+      { title: 'Matplotlib' },
+      { title: 'Seaborn' },
+      { title: 'Scikit-learn' },
+      { title: 'Google Analytics' },
+      { title: 'Google Tag Manager' },
+      { title: 'Google Ads' },
+      { title: 'GoJs' },
+    ],
+    Other: [
+      { title: 'npm | yarn' },
+      { title: 'GitHub Pages' },
+      { title: 'Git' },
+      { title: 'Webpack' },
+      { title: 'Gulp' },
+      { title: 'Laravel Mix' },
+      { title: 'Babel' },
+      { title: 'ESLint' },
+      { title: 'Prettier' },
+      { title: 'Jest' },
+      { title: '1С' },
+      { title: 'Bitrix24' },
+      { title: 'Wordpress' },
+      { title: 'Jira' },
+      { title: 'Confluence' },
+      { title: 'Trello' },
+      { title: 'Slack' },
+      { title: 'EditorConfig' },
+      { title: 'Json2ts' },
+      { title: 'Cors' },
+      { title: 'Codepen' },
+      { title: 'Codesandbox' },
+      { title: 'Codesandbox' },
+      { title: 'svgomg' },
+      { title: 'imagecompressor' },
+      { title: 'Autoprefixer CSS' },
+      { title: 'CSS Minifier' },
+      { title: 'Tabmine ' },
+    ],
+  }
 
-  const backend = [
-    { title: 'NodeJS' },
-    { title: 'Express' },
-    { title: 'RESTful API' },
-    { title: 'SQL' },
-    { title: 'PostgreSQL' },
-    { title: 'Firebase' },
-    { title: 'Supabase' },
-    { title: 'MongoDB' },
-    { title: 'Mongoose' },
-    { title: 'GraphQL' },
-    { title: 'Apollo Client' },
-    { title: 'Apollo Server' },
-  ]
-
-  const uiux = [
-    { title: 'Figma' },
-    { title: 'Framer motion' },
-    { title: 'PixiJS' },
-    { title: 'Materialize' },
-    { title: 'Ant Design of React' },
-    { title: 'Material-UI' },
-    { title: 'Material Design 3' },
-    { title: 'React-Vis' },
-    { title: 'UIkit' },
-    { title: 'Chart.js' },
-    { title: 'React-Motion' },
-    { title: 'React Materialize' },
-    { title: 'React Bootstrap' },
-    { title: 'React-Spring' },
-    { title: 'Tailblocks' },
-  ]
-
-  const analytics = [
-    { title: 'Python' },
-    { title: 'Amplitude' },
-    { title: 'Tableau' },
-    { title: 'Power BL' },
-    { title: 'Highcharts' },
-    { title: 'GoJs' },
-    { title: 'Google Excel' },
-    { title: 'Pandas' },
-    { title: 'Numpy' },
-    { title: 'Matplotlib' },
-    { title: 'Seaborn' },
-    { title: 'Scikit-learn' },
-    { title: 'Google Analytics' },
-    { title: 'Google Tag Manager' },
-    { title: 'Google Ads' },
-    { title: 'GoJs' },
-  ]
-  const Other = [
-    { title: 'npm | yarn' },
-    { title: 'GitHub Pages' },
-    { title: 'Git' },
-    { title: 'Webpack' },
-    { title: 'Gulp' },
-    { title: 'Laravel Mix' },
-    { title: 'Babel' },
-    { title: 'ESLint' },
-    { title: 'Prettier' },
-    { title: 'Jest' },
-    { title: '1С' },
-    { title: 'Bitrix24' },
-    { title: 'Wordpress' },
-    { title: 'Jira' },
-    { title: 'Confluence' },
-    { title: 'Trello' },
-    { title: 'Slack' },
-    { title: 'EditorConfig' },
-    { title: 'Json2ts' },
-    { title: 'Cors' },
-    { title: 'Codepen' },
-    { title: 'Codesandbox' },
-    { title: 'Codesandbox' },
-    { title: 'svgomg' },
-    { title: 'imagecompressor' },
-    { title: 'Autoprefixer CSS' },
-    { title: 'CSS Minifier' },
-    { title: 'Tabmine ' },
-  ]
   return (
     <div>
       <ul className={s.skills}>
         <div>
           <h2>Frontend</h2>
-          {frontend.map((el, i) => (
+          {skills.frontend.map((el, i) => (
             <motion.li
               key={i}
               initial={{ opacity: 0, y: -20 }}
@@ -120,7 +127,7 @@ const Skills = () => {
         </div>
         <div>
           <h2>UX / UI</h2>
-          {uiux.map((el, i) => (
+          {skills.uiux.map((el, i) => (
             <motion.li
               key={i}
               initial={{ opacity: 0, y: +20 }}
@@ -133,7 +140,7 @@ const Skills = () => {
         </div>
         <div>
           <h2>Backend</h2>
-          {backend.map((el, i) => (
+          {skills.backend.map((el, i) => (
             <motion.li
               key={i}
               initial={{ opacity: 0, y: +20 }}
@@ -146,7 +153,7 @@ const Skills = () => {
         </div>
         <div>
           <h2>Analytics</h2>
-          {analytics.map((el, i) => (
+          {skills.analytics.map((el, i) => (
             <motion.li
               key={i}
               initial={{ opacity: 0, y: +20 }}
@@ -159,7 +166,7 @@ const Skills = () => {
         </div>
         <div>
           <h2>Other</h2>
-          {Other.map((el, i) => (
+          {skills.Other.map((el, i) => (
             <motion.li
               key={i}
               initial={{ opacity: 0, y: +20 }}
