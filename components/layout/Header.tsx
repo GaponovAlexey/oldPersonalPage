0a2127ec3379efa3551907a3abeb7797be3bdca7
Kekelberi <alexei.gaponov@gmail.com>
2022-04-13 20:57:35 +0300

    save

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
           <Link href='/workShop'>
             <li>Workshop</li>
           </Link>
-          <Link href='/Skills'>
+          <Link href='/skills'>
             <li>Skills</li>
           </Link>
           <motion.li
--- pages/Skills.tsx
+++ pages/Skills.tsx
@@ -1,5 +1,61 @@
 const Skills = () => {
-  return <div>Skills</div>
+  return (
+    <div>
+      {/* <h1>Skills</h1> */}
+      <ul className='flex justify-between'>
+        <div>
+          <h2>Frontend</h2>
+          <li>JS | TS</li>
+          <li>HTML | CSS</li>
+          <li>React</li>
+          <li>React Context API</li>
+          <li>Redux Toolkit</li>
+          <li>RTK Query</li>
+          <li>Redux Saga</li>
+          <li>Redux Persist </li>
+          <li>Styled-Components</li>
+          <li>Sass</li>
+          <li>Git</li>
+          <li>GitHub Pages</li>
+          <li>Webpack</li>
+          <li>Babel</li>
+        </div>
+        <div>
+          <h2>Backend</h2>
+          <li>JS | TS</li>
+          <li>NodeJs</li>
+          <li>NextJs</li>
+          <li>Express</li>
+          <li>SQL</li>
+          <li>MongoDB</li>
+          <li>PostgreSQL</li>
+          <li>Firebase </li>
+        </div>
+        <div>
+          <h2>Algorithms</h2>
+        </div>
+        <div>
+          <h2>Analytics</h2>
+          <li>Amplitude</li>
+          <li>Tableau</li>
+          <li>Power BL</li>
+          <li>Google Excel</li>
+          <li>Python</li>
+          <li>Pandas</li>
+          <li>Numpy</li>
+          <li>Matplotlib</li>
+          <li>Seaborn</li>
+          <li>Scikit-learn</li>
+          <li>Google Analytics</li>
+          <li>Google Tag Manager</li>
+          <li>Google Ads</li>
+        </div>
+        <div>
+          <h2>Other</h2>
+        </div>
+      </ul>
+    </div>
+  )
 }
 
 export default Skills
